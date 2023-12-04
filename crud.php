6903079fbb2075d81d936cb50946ba23a5452ea9
RadityaCahyoAdhi <raditya.c.a@mail.ugm.ac.id>
2023-12-04 07:03:37 +0700

    updated username and password

--- crud.php
+++ crud.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "tesintegra";
+$password = "integra123";
 $dbname = "kependudukandb";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
