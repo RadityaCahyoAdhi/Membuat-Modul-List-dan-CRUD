994ec49638effb5aecb29612ad202651e7ea4f52
RadityaCahyoAdhi <raditya.c.a@mail.ugm.ac.id>
2023-12-04 08:14:22 +0700

    deleted comments

--- list.php
+++ list.php
@@ -731,26 +731,6 @@ require 'crud.php';
             xhr.send();
         }
         
-        // function sortFunction() {
-        //     var input, filter, table, tr, td, i, txtValue;
-        //     input = document.getElementById("urut-list");
-        //     filter = input.value.toUpperCase();
-        //     table = document.getElementsByTagName("table")[0];
-        //     tr = table.getElementsByTagName("tr");
-        //     for (i = 0; i < tr.length; i++) {
-        //         td = tr[i].getElementsByTagName("td")[1];
-        //         if (td) {
-        //         txtValue = td.textContent || td.innerText;
-        //         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        //             tr[i].style.display = "";
-        //         } 
-        //         else {
-        //             tr[i].style.display = "none";
-        //         }
-        //         }       
-        //     }
-        // }
-       
     </script>
 </body>
 </html>
